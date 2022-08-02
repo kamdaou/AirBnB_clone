ba59070cd515ac6dd6273da6d1b479f41e268850
kamdaou <you@example.com>
2022-08-02 12:35:14 +0200

    add test cases for the BaseModel class

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Tests for the base_model module
+"""
+import datetime
+import unittest
+
+from freezegun import freeze_time
+
+from models.base_model import BaseModel
+
+
+@freeze_time("2022-04-24")
+class TestBaseModel(unittest.TestCase):
+    """
+    TestBaseModel - Contains method that implement test cases
+    Methods:
+        test_create_instance: test if an instance of class BaseModel can be created
+        test_dict: Test the to_dic method of BaseModel
+    """
+
+    def test_create_instance(self):
+        """
+        test if an instance of class BaseModel can be created
+        and test its values.
+        """
+        my_base_model = BaseModel()
+        my_second_base_model = BaseModel()
+        self.assertIsInstance(my_base_model.created_at, datetime.datetime)
+        self.assertIsInstance(my_base_model.updated_at, datetime.datetime)
+        self.assertEqual(my_base_model.created_at, datetime.datetime(2022, 4, 24))
+        self.assertEqual(my_base_model.updated_at, datetime.datetime(2022, 4, 24))
+        self.assertNotEqual(my_base_model.id, my_second_base_model.id)
+
+    def test_dict(self):
+        """Test the to_dic method of BaseModel"""
+        my_base_model = BaseModel()
+        my_dic = my_base_model.to_dict()
+        custom_dic = {
+            'id': my_dic.get('id'),
+            'created_at': datetime.datetime(2022, 4, 24).isoformat(),
+            'updated_at': datetime.datetime(2022, 4, 24).isoformat(),
+            '__class__': 'BaseModel',
+        }
+        self.assertEqual(my_dic, custom_dic)
