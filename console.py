69b8305c99cfc4bd3d959ce717574f698e1f7f1f
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-08-08 01:24:11 +0300

    Console 0.1.1

--- console.py
+++ console.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 """Console module"""
 import cmd
-import models
+import shlex
 import json
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
     class_names = {
-            "BaseModel": models.BaseModel,
-            "User": models.User,
-            "State": models.State,
-            "City": models.City,
-            "Amenity": models.Amenity,
-            "Place": models.Place,
-            "Review": models.Review
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
             }
     class_instances = {}
 
@@ -25,7 +32,7 @@ class HBNBCommand(cmd.Cmd):
         """
         raise SystemExit
 
-    def do_EOF(self, arg):
+    def do_EOF(self, args):
         """Exit the program"""
         raise SystemExit
 
@@ -40,56 +47,59 @@ class HBNBCommand(cmd.Cmd):
         else:
             self.__create(class_name)
 
-    def do_show(self, class_name):
-        if not class_name:
+    def do_show(self, arg):
+        args = shlex.split(arg)
+        key = args[0] + "." + args[1]
+        HBNBCommand.class_instances = storage.all()
+        if not args[0]:
             print("** class name missing **")
-        elif self.__is_class(class_name):
+        elif self.__is_class(args[0]):
             print("** class doesn't exist **")
-        elif self.__has_id(class_name):
+        elif self.__has_id(key):
             print("** instance id missing **")
-        elif self.__is_class_found(class_name):
+        elif self.__is_class_found(key):
             print("** no instance found **")
         else:
-            HBNBCommand.class_instances = storage.all()
-            self.__show(class_name)
+            self.__show(args[0] + "." + args[1])
 
-    def do_destroy(self, class_name):
-        if not class_name:
+    def do_destroy(self, arg):
+        args = shlex.split(arg)
+        key = args[0] + "." + args[1]
+        HBNBCommand.class_instances = storage.all()
+        if not args[0]:
             print("** class name missing **")
-        elif self.__is_class(class_name):
+        elif self.__is_class(args[0]):
             print("** class doesn't exist **")
-        elif self.__has_id(class_name):
+        elif self.__has_id(key):
             print("** instance id missing **")
-        elif self.__is_class_found(class_name):
+        elif self.__is_class_found(key):
             print("** no instance found **")
         else:
-            HBNBCommand.class_instances = storage.all()
-            self.__destroy(class_name)
+            self.__destroy(args[0] + "." + args[1])
 
     def do_all(self, class_name):
         HBNBCommand.class_instances = storage.all()
-        if class_name not in self.class_names:
-            print("** class doesn't exist **")
-        elif not class_name:
+        if not class_name:
             self.__all()
+        elif class_name not in self.class_names:
+            print("** class doesn't exist **")
         else:
             self.__all_of(class_name)
 
-    def do_update(self, class_name, id, attribute_name, attribute_value):
-        if not class_name:
+    def do_update(self, arg):
+        args = shlex.split(arg)
+        key = args[0] + "." + args[1]
+        HBNBCommand.class_instances = storage.all()
+        if not args[0]:
             print("** class name missing **")
-        elif self.__is_class(class_name):
+        elif self.__is_class(args[0]):
             print("** class doesn't exist **")
-        elif self.__has_id(class_name):
+        elif self.__has_id(key):
             print("** instance id missing **")
-        elif self.__is_class_found(class_name):
-            print("** no instance found **")
-        elif self.__is_class_found(class_name):
+        elif self.__is_class_found(key):
             print("** no instance found **")
         else:
-            HBNBCommand.class_instances = storage.all()
-            key = class_name + "." + id
-            self.__update(key, attribute_name, attribute_value)
+            self.__update(key + "." + args[0], args[2], args[3])
 
     @staticmethod
     def __is_class(class_name):
@@ -97,32 +107,36 @@ class HBNBCommand(cmd.Cmd):
 
     @staticmethod
     def __has_id(class_name):
-        return not HBNBCommand.class_instances[class_name].id
+        return not HBNBCommand.class_instances[class_name]
 
     @staticmethod
     def __is_class_found(class_name):
+        print(HBNBCommand.class_instances)
         return class_name not in HBNBCommand.class_instances
 
     @staticmethod
     def __create(class_name):
         class_instance = HBNBCommand.class_names[class_name]()
         class_instance.save()
-        print(class_instance.id())
+        print(class_instance.id)
 
     @staticmethod
     def __show(class_name):
         print(str(HBNBCommand.class_instances[class_name]))
 
+    @staticmethod
     def __destroy(class_name):
         del HBNBCommand.class_instances[class_name]
         storage.save()
 
+    @staticmethod
     def __all():
         res = []
         for key, value in HBNBCommand.class_instances.items():
             res.append(str(value))
         print(json.dumps(res))
 
+    @staticmethod
     def __all_of(class_name):
         res = []
         for key, value in HBNBCommand.class_instances.items():
@@ -130,6 +144,7 @@ class HBNBCommand(cmd.Cmd):
                 res.append(str(value))
             print(json.dumps(res))
 
+    @staticmethod
     def __update(key, attribute_name, attribute_value):
         temp = HBNBCommand.class_instances[key].__dict__
         if attribute_name in temp.keys():
